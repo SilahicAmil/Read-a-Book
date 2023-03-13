619585fb839633fe1eb367b707f6ea372f54dae3
Amil Silahic <jinobie02@gmail.com>
2023-03-13 11:18:40 -0600

    feat: :art: Adding audioFile fetching on audioplayer screen

--- screens/AudioPlayerScreen.js
+++ screens/AudioPlayerScreen.js
@@ -1,9 +1,30 @@
 import { Button, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+
+import { supabase } from "../lib/supabase";
 
 const AudioPlayerScreen = ({ navigation, route }) => {
-  console.log(route.params.title);
+  const [audioFiles, setAudioFiles] = useState([]);
+
+  useEffect(() => {
+    const fetchAudioFiles = async () => {
+      const { data, error } = await supabase.storage
+        .from("audiobooks")
+        // update this to route.params.title
+        .list("Foolish Dictionary", {
+          offset: 0,
+          sortBy: { column: "name", order: "asc" },
+        });
+      setAudioFiles(data);
+    };
+    fetchAudioFiles();
+  }, []);
+
   return (
     <>
+      {audioFiles.map((item) => {
+        return <Text key={item.id}>{item.name}</Text>;
+      })}
       <View>
         <Text>Audio Player Screen</Text>
         <Button title="Go Back" onPress={() => navigation.goBack()} />
