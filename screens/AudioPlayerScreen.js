c483182b42a987ef6577b48db49252de93823cfd
Amil Silahic <jinobie02@gmail.com>
2023-03-13 22:37:36 -0600

    updating audio player screen to show basic loading state

--- screens/AudioPlayerScreen.js
+++ screens/AudioPlayerScreen.js
@@ -1,12 +1,15 @@
 import { Button, Text, View } from "react-native";
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 
+import LoadingItem from "../components/UI/LoadingItem";
 import { supabase } from "../lib/supabase";
 
 const AudioPlayerScreen = ({ navigation, route }) => {
   const [audioFiles, setAudioFiles] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     const fetchAudioFiles = async () => {
       const { data, error } = await supabase.storage
         .from("audiobooks")
@@ -15,18 +18,31 @@ const AudioPlayerScreen = ({ navigation, route }) => {
           sortBy: { column: "name", order: "asc" },
         });
       setAudioFiles(data);
+      setIsLoading(false);
     };
     fetchAudioFiles();
   }, []);
 
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return <Button title="Go Back" onPress={() => navigation.goBack()} />;
+      },
+    });
+  }, []);
+
   return (
     <>
-      {audioFiles.map((item) => {
-        return <Text key={item.id}>{item.name}</Text>;
-      })}
+      {isLoading ? (
+        <LoadingItem />
+      ) : (
+        audioFiles.map((item) => {
+          return <Text key={item.id}>{item.name}</Text>;
+        })
+      )}
+
       <View>
         <Text>Audio Player Screen</Text>
-        <Button title="Go Back" onPress={() => navigation.goBack()} />
       </View>
     </>
   );
