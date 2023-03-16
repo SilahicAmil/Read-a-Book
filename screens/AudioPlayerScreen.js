0a6a40211a8c9d5a92dae13babc68b6c9acb5562
Amil Silahic <jinobie02@gmail.com>
2023-03-16 09:28:48 -0600

    feat: :zap: Adding section numbers to audio player screen and minor styling updates

--- screens/AudioPlayerScreen.js
+++ screens/AudioPlayerScreen.js
@@ -1,4 +1,11 @@
-import { Button, FlatList, Text, View } from "react-native";
+import {
+  Button,
+  FlatList,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { useEffect, useLayoutEffect, useState } from "react";
 
 import { Audio } from "expo-av";
@@ -57,24 +64,53 @@ const AudioPlayerScreen = ({ navigation, route }) => {
       : undefined;
   }, [sound]);
 
+  const renderAudioItem = (itemData) => {};
+
   return (
     <>
-      <View>
-        <Text>Audio Player Screen</Text>
-        <FlatList
-          data={audioFiles}
-          renderItem={({ item }) => {
-            return (
-              <Button
-                title={item.name}
-                onPress={() => audioFileToPlayHandler(item.name)}
-              />
-            );
-          }}
-        />
+      <View style={styles.rootContainer}>
+        <View style={styles.listContainer}>
+          <FlatList
+            data={audioFiles}
+            renderItem={({ item, index }) => {
+              return (
+                <View style={styles.actionContainer}>
+                  <Pressable
+                    onPress={() => audioFileToPlayHandler(item.name)}
+                    style={styles.action}
+                  >
+                    <Text>{`Section {${index + 1}}`}</Text>
+                  </Pressable>
+                </View>
+              );
+            }}
+            keyExtractor={(item) => item.id}
+          />
+        </View>
       </View>
     </>
   );
 };
 
 export default AudioPlayerScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    display: "flex",
+    width: "100%",
+    height: "100%",
+    flex: 1,
+  },
+  listContainer: {
+    height: "50%",
+    padding: 8,
+  },
+  actionContainer: {
+    width: "100%",
+  },
+  action: {
+    height: 42,
+    backgroundColor: "white",
+  },
+  actionText: {},
+});
