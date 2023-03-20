6bd8d6ba62d8c786f902f0cee03c522e00647faf
Amil Silahic <jinobie02@gmail.com>
2023-03-20 11:20:58 -0600

    feat: :art: Implementing favorite books card
    Implementing favorite books card and getting the data from the favoritesContext - below favorites will app like a typical accordion of FAQ and dnation button at bottom

--- components/UserProfile/UserFavoritesCard.js
+++ components/UserProfile/UserFavoritesCard.js
@@ -1,12 +1,64 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
-const UserFavoritesCard = ({}) => {
-  // pass or use context here idk yet
+import { Card } from "@rneui/themed";
+
+const UserFavoritesCard = ({ bookName }) => {
   return (
-    <View>
-      <View></View>
+    <View style={styles.rootContainer}>
+      <View style={styles.cardContainer}>
+        <Card containerStyle={styles.card}>
+          <Card.Image
+            style={styles.image}
+            source={{
+              uri: "https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+            }}
+          />
+          <Card.Title style={styles.title}>{bookName}</Card.Title>
+        </Card>
+      </View>
     </View>
   );
 };
 
 export default UserFavoritesCard;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    height: "100%",
+    width: "100%",
+  },
+  cardContainer: {
+    padding: 10,
+    marginRight: 8,
+    width: 175,
+    height: 300,
+    gap: 12,
+  },
+  card: {
+    padding: 5,
+    margin: 8,
+    width: "100%",
+    height: "100%",
+    borderRadius: 8,
+    borderColor: "rgba(000, 000, 000, .6)",
+    borderWidth: 1,
+    shadowColor: "black",
+    shadowOffset: {
+      width: 3,
+      height: 3,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+    elevation: 6,
+  },
+  image: {
+    height: "90%",
+    width: "100%",
+  },
+  title: {
+    fontSize: 12,
+    fontFamily: "Kantumury-Pro",
+    color: "black",
+  },
+});
--- screens/UserProfileScreen.js
+++ screens/UserProfileScreen.js
@@ -1,19 +1,29 @@
 import { ScrollView, StyleSheet, Text } from "react-native";
 
 import { FavoritesContext } from "../store/context/favorites-context";
+import { FlatList } from "react-native-gesture-handler";
 import UserFavoritesCard from "../components/UserProfile/UserFavoritesCard";
 import UserHeader from "../components/UserProfile/UserHeader";
 import { useContext } from "react";
 
 const UserProfileScreen = ({}) => {
   const favoriteBookCtx = useContext(FavoritesContext);
+  const books = favoriteBookCtx.bookNames;
+  console.log(books);
 
   return (
     <>
       <ScrollView style={styles.rootContainer}>
         <UserHeader />
-        <Text>Favorite Books:{favoriteBookCtx.bookNames}</Text>
-        <UserFavoritesCard />
+        <Text style={styles.title}>Favorite Books:</Text>
+        <FlatList
+          data={books}
+          renderItem={({ item }) => {
+            return <UserFavoritesCard bookName={item} />;
+          }}
+          rootContainer
+          horizontal
+        />
       </ScrollView>
     </>
   );
@@ -24,6 +34,10 @@ export default UserProfileScreen;
 const styles = StyleSheet.create({
   rootContainer: {
     backgroundColor: "#FBF8F2",
-    height: "100%",
+  },
+  title: {
+    fontSize: 26,
+    fontFamily: "DMSerif-Display",
+    marginLeft: 12,
   },
 });
