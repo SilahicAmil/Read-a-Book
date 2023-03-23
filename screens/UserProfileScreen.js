a3f9347b61ce6caedc21bb4b4757878379bce152
Amil Silahic <jinobie02@gmail.com>
2023-03-23 14:40:13 -0600

    updating empty container styling again

--- screens/UserProfileScreen.js
+++ screens/UserProfileScreen.js
@@ -46,7 +46,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#FBF8F2",
   },
   emptyContainer: {
-    height: "20%",
+    height: "90%",
     display: "flex",
     justifyContent: "center",
   },
