e70bdf1e350509723e78c99a0c8d240e7c4f28a1
Amil Silahic <jinobie02@gmail.com>
2023-04-11 13:02:01 -0600

    misc

--- screens/AllBooksScreen.js
+++ screens/AllBooksScreen.js
@@ -1,4 +1,4 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 
 import BooksCard from "../components/AllBooks/BooksCard";
 import ErrorItem from "../components/UI/ErrorItem";
